971b7251cfd701623149af454f64b45abeac5cfc
Nadiar AS <codxse@gmail.com>
2016-02-19 10:14:28 +0700

    setup app module and controller

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Nadiar on 2/19/16.
+ */
+(function() {
+    var app = angular.module('store', []);
+    app.controller('StoreController', function() {
+        this.product = gem;
+    });
+
+    var gem = {
+        name: 'Deodorant',
+        price: 2.95,
+        description: 'A deodorant is a substance applied to the body to prevent body odor caused by the bacterial breakdown of perspiration in armpits, feet, and other areas of the body. A subgroup of deodorants, antiperspirants, affect odor as well as prevent sweating by affecting sweat glands.'
+    }
+}());
